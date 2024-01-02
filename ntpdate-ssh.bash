d70b4123f47be47cef1b8e69879439579e1d7bf3
Luandre Bernardi de Andrade <luandre.andrade@grupopereira.local>
2024-01-02 10:25:57 -0300

    commit

---
+++ ntpdate-ssh.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Criação: Luandre B. Andrade
+
+# Função de conexão sshpass
+function ssh_conn() {
+    local user="root"
+    local password="wpsbrasil"
+    local noKey="-oStrictHostKeyChecking=no"
+    sshpass -p "$password" ssh "$noKey" "$user"@"$1" "$2"
+}
+
+# Ajusta o horario dos terminais.
+function ntpdate_c() {
+    ssh_conn $1 "ntpdate -s 192.168.6.140"
+}
+
+function init() {
+# Quantidade de terminais
+    local quantidade=5
+    local data=$(date +"%T")
+    echo -e "[ $data ]> Inicio do ciclo."
+    for ((count = 2; count <= quantidade; count++)); do
+        local e=1
+        local c=10
+        local ip="192.168.6.14"
+        local faixa="$ip$count"
+            ping -c 2 "$ip$count" > /dev/null
+             if [ $? -ne 0 ]; then
+                echo -e "[ $data ]> ConexÃ£o com o IP - $faixa Falhou!"
+             else
+                echo -e "[ $data ]> $faixa Ping OK!"
+                ntpdate_c $faixa
+             fi
+    done
+    echo -e "[ $data ]> Fim do ciclo."
+}
+
+# Repete a cada 10 min.
+while true
+ do
+   init
+   sleep 600
+ done
